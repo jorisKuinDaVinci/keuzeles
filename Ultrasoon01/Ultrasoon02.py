4804f23ca3fec0572d886ef59ca0ead0359709f7
jorisKuinDaVinci <99074486@mydavinci.nl>
2023-12-19 10:02:39 +0100

    ultrasoon opdracht 2 begonnen

--- Ultrasoon01/Ultrasoon02.py
+++ Ultrasoon01/Ultrasoon02.py
@@ -3,3 +3,15 @@ from time import sleep
 
 class Servo:
     def __init__(self, MIN_DUTY=300000, MAX_DUTY=2300000, pin=0, freq=50):
+        self.pwm = machine.PWM(machine.Pin(pin))
+        self.pwm.freq(freq)
+        self.MIN_DUTY = MIN_DUTY
+        self.MAX_DUTY = MAX_DUTY
+        
+    def rotateDeg(self, deg):
+        deg = max(0, min(deg, 180))
+        duty_ns = int(self.MAX_DUTY - deg * (self.MAX_DUTY-self.MIN_DUTY)/180)
+        self.pwm.duty_ns(duty_ns)
+        
+servo = Servo(pin=28)
+servo.rotateDeg(90)
